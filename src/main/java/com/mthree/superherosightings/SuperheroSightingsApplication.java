d63f5e8a7430b0dc948ad9b1e7f40adee638d04a
Steven <steven.686295@gmail.com>
2021-08-24 15:10:23 -0400

    Setup initial project and completed db schema

---
+++ src/main/java/com/mthree/superherosightings/SuperheroSightingsApplication.java
@@ -0,0 +1,11 @@
+package com.mthree.superherosightings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroSightingsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheroSightingsApplication.class, args);
+    }
+}
